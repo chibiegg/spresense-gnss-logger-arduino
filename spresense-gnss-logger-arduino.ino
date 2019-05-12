52d48e8f813c9b0f6479a5c5b142f0f2353c6f15
Youta EGUSA <y-egusa@sakura.ad.jp>
2019-05-12 17:07:21 +0900

    NMEA Libraryを導入

--- spresense-gnss-logger-arduino.ino
+++ spresense-gnss-logger-arduino.ino
@@ -1,10 +1,23 @@
 
 /* include the GNSS library */
 #include <GNSS.h>
+#include <GNSSPositionData.h>
+#include "gpsutils/cxd56_gnss_nmea.h"
 
 #define STRING_BUFFER_SIZE  128       /**< %Buffer size */
+char StringBuffer[STRING_BUFFER_SIZE];
 
 static SpGnss Gnss;                   /**< SpGnss object */
+
+// NMEA
+FILE *fp = NULL;
+char current_filename[100];
+NMEA_OUTPUT_CB funcs;
+char nmea_buf[NMEA_SENTENCE_MAX_LEN];
+FAR static char *reqbuf(uint16_t size);
+static void freebuf(FAR char *buf);
+static int outnmea(FAR char *buf);
+static int outbin(FAR char *buf, uint32_t len);
 
 /**
  * @brief Turn on / off the LED0 for CPU active notification.
@@ -130,6 +143,8 @@ void setup() {
     Led_isError(true);
     exit(0);
   }
+
+  log_init();
 }
 
 /**
@@ -137,7 +152,6 @@ void setup() {
  */
 static void print_pos(SpNavData *pNavData)
 {
-  char StringBuffer[STRING_BUFFER_SIZE];
 
   /* print time */
   snprintf(StringBuffer, STRING_BUFFER_SIZE, "%04d/%02d/%02d ", pNavData->time.year, pNavData->time.month, pNavData->time.day);
@@ -233,6 +247,92 @@ static void print_condition(SpNavData *pNavData)
   }
 }
 
+
+void log_init(){
+  NMEA_InitMask();
+  NMEA_SetMask(0x000040ff);
+  funcs.bufReq  = reqbuf;
+  funcs.out     = outnmea;
+  funcs.outBin  = outbin;
+  funcs.bufFree = freebuf;
+  NMEA_RegistOutputFunc(&funcs);
+}
+
+void log_open(char *filename){
+  printf("Open %s\n", filename);
+  fp = fopen(filename, "a");
+  if (fp == NULL) {
+    printf("Open error %s\n", filename);
+  }
+}
+
+
+void log_write(struct cxd56_gnss_positiondata_s *posdatp){
+
+  snprintf(
+    StringBuffer,
+    STRING_BUFFER_SIZE,
+    "%04d/%02d/%02d %02d:%02d:%02d",
+    posdatp->receiver.date.year, posdatp->receiver.date.month, posdatp->receiver.date.day,
+    posdatp->receiver.time.hour, posdatp->receiver.time.minute, posdatp->receiver.time.sec
+  );
+  Serial.println(StringBuffer);
+  
+  if (fp==NULL && posdatp->receiver.date.year>2000) {
+    sprintf(
+      current_filename, "/mnt/sd0/%04d%02d%02d-%02d%02d%02d.nmea",
+      posdatp->receiver.date.year, posdatp->receiver.date.month, posdatp->receiver.date.day,
+      posdatp->receiver.time.hour, posdatp->receiver.time.minute, posdatp->receiver.time.sec
+    );
+    log_open(current_filename);
+  }
+  NMEA_Output(posdatp);
+  if (fp!=NULL) {
+    if(posdatp->receiver.time.sec == 59){
+      fflush(fp);
+      int fd = fileno(fp);
+      fsync(fd);
+    }
+    if(posdatp->receiver.time.minute == 59 && posdatp->receiver.time.sec == 59){
+      fclose(fp);
+      fp = NULL;
+    }
+  }
+}
+
+/* output NMEA */
+FAR static char *reqbuf(uint16_t size)
+{
+  if (size > sizeof(nmea_buf))
+    {
+      snprintf(StringBuffer, STRING_BUFFER_SIZE, "reqbuf error: oversize %s\n", size);
+      Serial.println(StringBuffer);
+      return NULL;
+    }
+  return nmea_buf;
+}
+
+static void freebuf(FAR char *buf)
+{
+}
+
+static int outnmea(FAR char *buf)
+{
+  int ret = 0;
+  if (fp != NULL){
+    ret = fputs(buf, fp);
+  }
+  Serial.print(buf);
+  return ret;
+}
+
+static int outbin(FAR char *buf, uint32_t len)
+{
+  return len;
+  //return write(WRITE_FD, buf, (size_t)len);
+}
+
+
 /**
  * @brief %Print position information and satellite condition.
  *
@@ -269,6 +369,13 @@ void loop()
 
     /* Print position information. */
     print_pos(&NavData);
+
+
+    GnssPositionData PosData;
+    Gnss.getPositionData((char *)&PosData);
+
+    struct cxd56_gnss_positiondata_s *cxd56_gnss_positiondata_p = (struct cxd56_gnss_positiondata_s *)&PosData.Data;
+    log_write(cxd56_gnss_positiondata_p);
   }
   else
   {
@@ -277,4 +384,6 @@ void loop()
   }
 
 }
+
+
 
